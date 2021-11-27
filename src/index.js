33ece53935414c75842e17a3418a7c65c11db3e6
alexgitub <alex08585@gmail.com>
2021-11-28 01:29:49 +0800

    Update stuff.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {BrowserRouter as Router} from 'react-router-dom'
-import App from './App'
-import { initContract } from './utils'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {HashRouter as Router} from 'react-router-dom';
+import App from './App';
+import { initContract } from './utils';
 
 window.nearInitPromise = initContract()
   .then(() => {
